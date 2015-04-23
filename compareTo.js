7f00b136e2bafb5a6fdb29fe2eb22efad6503c6f
Alex Booker <alexbooker@fastmail.im>
2015-04-23 16:21:18 +0100

    added immediately-invoked function expression

--- compareTo.js
+++ compareTo.js
@@ -1,45 +1,49 @@
-function validatePassword(target, validator) {  
-	if( validator.value !== target.value ) {
-		validator.setCustomValidity("These passwords don't match.");
-	} else {
-		validator.setCustomValidity('');
+(function() {
+
+	function validatePassword(target, validator) {  
+		if( validator.value !== target.value ) {
+			validator.setCustomValidity("These passwords don't match.");
+		} else {
+			validator.setCustomValidity('');
+		}
+
 	}
 
-}
 
+	function setCompareTo() {  //sets up all elements of compareTo
+		var allInputs = document.getElementsByTagName('input');
+		for( var i=0; i<allInputs.length; i++ ) {
 
-function setCompareTo() {  //sets up all elements of compareTo
-	var allInputs = document.getElementsByTagName('input');
-	for( var i=0; i<allInputs.length; i++ ) {
+			if ( allInputs[i].hasAttribute('compareTo') ) {
+				var targetname = allInputs[i].getAttribute('compareTo');
+				var target = document.getElementsByName(targetname);
 
-		if ( allInputs[i].hasAttribute('compareTo') ) {
-			var targetname = allInputs[i].getAttribute('compareTo');
-			var target = document.getElementsByName(targetname);
+				if( target.length === 1 ) {  //make sure we reference exactly one element
+					allInputs[i].addEventListener("change", (function(target, validator) {  //closure loop
+							return function(){
+								validatePassword(target, validator);
+							};
+						})(target[0], allInputs[i])
+					);
 
-			if( target.length === 1 ) {  //make sure we reference exactly one element
-				allInputs[i].addEventListener("change", (function(target, validator) {  //closure loop
-						return function(){
-							validatePassword(target, validator);
-						};
-					})(target[0], allInputs[i])
-				);
+					target[0].addEventListener("change", (function(target, validator) {  //closure loop
+							return function(){
+								validatePassword(target, validator);
+							};
+						})(target[0], allInputs[i])
+					);
+					
 
-				target[0].addEventListener("change", (function(target, validator) {  //closure loop
-						return function(){
-							validatePassword(target, validator);
-						};
-					})(target[0], allInputs[i])
-				);
-				
+				}
 
 			}
+		
 
 		}
-	
-
 	}
-}
 
 
 
-setCompareTo();  //search for compareTo inputs and activate them
+	setCompareTo();  //search for compareTo inputs and activate them
+
+}());
