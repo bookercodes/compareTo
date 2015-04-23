ce43426c4295ceb268434c5fe92d7e71b4ad83d3
Alex Booker <alexbooker@fastmail.im>
2015-04-24 00:05:30 +0100

    readability tweaks

--- compareTo.js
+++ compareTo.js
@@ -11,32 +11,37 @@
   var inputs = document.getElementsByTagName('input');
   for (var i = 0; i < inputs.length; i++) {
     var input = inputs[i];
-    if (!input.hasAttribute('compareTo')) continue;
+    if (!input.hasAttribute('compareTo')) {
+      continue;
+    }
     var comparateName = input.getAttribute('compareTo');
     var comparates = document.getElementsByName(comparateName);
     if (comparates.length === 0) { 
-        console.error('CompareTo could not find an element with the name "' + comparateName + '". Please ensure that one element with that name exists.');  
-        continue; 
-     } else if (comparates.length > 1) {
-        console.error('CompareTo found more than one (' + comparates.length  + ') elements with the name "' + comparateName + '". Please ensure that only one element with that name exists.')
-        continue;
-     }
+      console.error('CompareTo could not find an element with the name "' + comparateName + '". Please ensure that one element with that name exists.');  
+      continue; 
+    }
+    if (comparates.length > 1) {
+      console.error('CompareTo found more than one (' + comparates.length  + ') elements with the name "' + comparateName + '". Please ensure that only one element with that name exists.')
+      continue;
+    }
     var comparate = comparates[0];
 
-    var defaultValidateError = 'These passwords don\'t match.';  //default validation error message
-    if (input.hasAttribute('compareToMsg')) { defaultValidateError = input.getAttribute('compareToMsg'); }  //set the validation error if attribute present
+    var error = 'These passwords don\'t match.';
+    if (input.hasAttribute('compareToMsg')) {
+      error = input.getAttribute('compareToMsg');
+    }
 
     input.addEventListener("change", (function(input, comparate, invalidmsg) {
       return function() {
         comparePasswords(input, comparate, invalidmsg);
       }
-    })(input, comparate, defaultValidateError));
+    })(input, comparate, error));
 
     comparate.addEventListener("change", (function(input, comparate, invalidmsg) {
       return function() {
         comparePasswords(input, comparate, invalidmsg);
       }
-    })(input, comparate, defaultValidateError));
+    })(input, comparate, error));
   }
 
 }());
