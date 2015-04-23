76db2b4c76317d76f7d91888faa58b234f2a428a
Gary B <Gary@GarysCorner.NET>
2015-04-23 08:48:38 -0500

    removed jQuery and allowed for multiple compareTo elements, closes #1

--- compareTo.js
+++ compareTo.js
@@ -1,17 +1,45 @@
-$(function() {
-  var confirm_password = $("input[compareTo]");
-  var comparateName = confirm_password.attr("compareTo");
-  var password = $("input[name=" + comparateName + "]");
+function validatePassword(target, validator) {  
+	if( validator.value !== target.value ) {
+		validator.setCustomValidity("These passwords don't match.");
+	} else {
+		validator.setCustomValidity('');
+	}
 
-  function validatePassword() {
-    if (confirm_password.val() !== password.val()) {
-      confirm_password.get(0).setCustomValidity("These passwords don't match.");
-    } else {
-      confirm_password.get(0).setCustomValidity('');
-    }
-  }
+}
 
-  confirm_password.on("change", validatePassword)
-  password.on("change", validatePassword);
 
-});
+function setCompareTo() {  //sets up all elements of compareTo
+	var allInputs = document.getElementsByTagName('input');
+	for( var i=0; i<allInputs.length; i++ ) {
+
+		if ( allInputs[i].hasAttribute('compareTo') ) {
+			var targetname = allInputs[i].getAttribute('compareTo');
+			var target = document.getElementsByName(targetname);
+
+			if( target.length === 1 ) {  //make sure we reference exactly one element
+				allInputs[i].addEventListener("change", (function(target, validator) {  //closure loop
+						return function(){
+							validatePassword(target, validator);
+						};
+					})(target[0], allInputs[i])
+				);
+
+				target[0].addEventListener("change", (function(target, validator) {  //closure loop
+						return function(){
+							validatePassword(target, validator);
+						};
+					})(target[0], allInputs[i])
+				);
+				
+
+			}
+
+		}
+	
+
+	}
+}
+
+
+
+setCompareTo();  //search for compareTo inputs and activate them
