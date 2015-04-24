0ce706eac44f9f2185aea119eaf3fa8ab391a92c
Gabin <hello@gabinaureche.com>
2015-04-25 00:19:19 +0200

    refactor, support data- attribute, use querySelectorAll to get relevant elements

--- compareTo.js
+++ compareTo.js
@@ -1,4 +1,5 @@
 (function() {
+  'use strict';
 
   function comparePasswords(password, comparate, invalidmsg) {
     if (password.value !== comparate.value) {
@@ -8,40 +9,34 @@
     }
   }
 
-  var inputs = document.getElementsByTagName('input');
-  for (var i = 0; i < inputs.length; i++) {
-    var input = inputs[i];
-    if (!input.hasAttribute('compareTo')) {
-      continue;
-    }
-    var comparateName = input.getAttribute('compareTo');
+  var inputs = document.querySelectorAll('input[compareTo]').concat(document.querySelectorAll('input[data-compareTo]'));
+
+  for (var i = 0, input; input = inputs[i]; i++) {
+    var comparateName = input.getAttribute('compareTo') || input.getAttribute('data-compareTo');
     var comparates = document.getElementsByName(comparateName);
+
     if (comparates.length === 0) {
       console.error('CompareTo could not find an element with the name "' + comparateName + '". Please ensure that one element with that name exists.');
       continue;
     }
+
     if (comparates.length > 1) {
       console.error('CompareTo found more than one (' + comparates.length  + ') elements with the name "' + comparateName + '". Please ensure that only one element with that name exists.')
       continue;
     }
+
     var comparate = comparates[0];
+    var error = input.getAttribute('compareToError')      ||
+                input.getAttribute('data-compareToError') ||
+                'These passwords don\'t match.';
 
-    var error = 'These passwords don\'t match.';
-    if (input.hasAttribute('compareToError')) {
-      error = input.getAttribute('compareToError');
-    }
-
-    input.addEventListener("change", (function(input, comparate, invalidmsg) {
+    var callback = (function(input, comparate, invalidmsg) {
       return function() {
         comparePasswords(input, comparate, invalidmsg);
       }
-    })(input, comparate, error));
+    })(input, comparate, error);
 
-    comparate.addEventListener("change", (function(input, comparate, invalidmsg) {
-      return function() {
-        comparePasswords(input, comparate, invalidmsg);
-      }
-    })(input, comparate, error));
+    input.addEventListener('change', callback);
+    comparate.addEventListener('change', callback);
   }
-
 }());
